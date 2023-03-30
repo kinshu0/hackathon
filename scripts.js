866480dc9404986436124b7fa170c3f0e1b4276d
Kinshu Gupta <kinshugupta2002@gmail.com>
2023-03-30 17:51:02 -0400

    good version one

---
+++ scripts.js
@@ -0,0 +1,43 @@
+// scripts.js
+function getTimeRemaining(endtime) {
+  const total = Date.parse(endtime) - Date.parse(new Date());
+  const seconds = Math.floor((total / 1000) % 60);
+  const minutes = Math.floor((total / 1000 / 60) % 60);
+  const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+  const days = Math.floor(total / (1000 * 60 * 60 * 24));
+
+  return {
+    total,
+    days,
+    hours,
+    minutes,
+    seconds
+  };
+}
+
+function initializeClock(id, endtime) {
+  const clock = document.getElementById(id);
+
+  function updateClock() {
+    const t = getTimeRemaining(endtime);
+
+    clock.innerHTML = (t.days > 0 ? t.days + 'd ' : '') +
+                      t.hours + 'h ' +
+                      t.minutes + 'm ' +
+                      t.seconds + 's';
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+      clock.innerHTML = "Hackathon Ended";
+    }
+  }
+
+  updateClock();
+  const timeinterval = setInterval(updateClock, 1000);
+}
+
+// Set the date and time of your hackathon
+const hackathonDate = new Date("2023-04-01T11:00:00");
+const hackathonDuration = 5 * 60 * 60 * 1000; // 5 hours
+const deadline = new Date(hackathonDate.getTime() + hackathonDuration);
+initializeClock('countdown', deadline);
